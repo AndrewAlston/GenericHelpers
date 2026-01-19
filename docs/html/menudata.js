69a03e7a40cd366225cf1dd101f5611a7c2ae6e9
Andrew <aa@alstonnetworks.net>
2026-01-19 19:08:19 +0400

    Rebuilt documentation

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -34,5 +34,15 @@ var menudata={children:[
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"Globals",url:"globals.html",children:[
-{text:"All",url:"globals.html"},
-{text:"Functions",url:"globals_func.html"}]}]}]}
+{text:"All",url:"globals.html",children:[
+{text:"c",url:"globals.html#index_c"},
+{text:"d",url:"globals.html#index_d"},
+{text:"f",url:"globals.html#index_f"},
+{text:"g",url:"globals.html#index_g"},
+{text:"i",url:"globals.html#index_i"},
+{text:"l",url:"globals.html#index_l"},
+{text:"m",url:"globals.html#index_m"},
+{text:"r",url:"globals.html#index_r"},
+{text:"s",url:"globals.html#index_s"}]},
+{text:"Functions",url:"globals_func.html"},
+{text:"Typedefs",url:"globals_type.html"}]}]}]}
